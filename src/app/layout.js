cfa2cc2acf11eccc5f556f9fb8fae556c8761fbd
Harsh Deep Rao <harshurao058@gmail.com>
2025-06-27 13:46:17 +0530

    Poppins Font added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
 'use client'
 
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google"
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SessionProvider } from 'next-auth/react'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+ 
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
   subsets: ["latin"],
-});
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-poppins",
+  display: "swap",
+})
+
 
  
 
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
 
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} antialiased`}
       >
 
        <SessionProvider>
