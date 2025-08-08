47e6f434ca5d09a27fbaaa7aee1441ae3c743721
Harsh Deep Rao <harshurao058@gmail.com>
2025-08-08 14:13:10 +0530

    Meta Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,44 @@
 "use client";
 
-// import { Poppins } from 'next /font/google';
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "next-auth/react";
-
-// const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600'] });
+import Script from "next/script";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-      // className={`${poppins.variable} antialiased`}
-      >
+      <head>
+        {/* Meta Pixel Script */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '775746001807500');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+      <body>
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=775746001807500&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <SessionProvider>
           <Navbar />
-
           {children}
           <Footer />
         </SessionProvider>
